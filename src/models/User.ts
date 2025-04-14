e9d8a06b9909f1d1ff29f5e29e524f5fad16d1f0
Alan <lic.sanchezalan@gmail.com>
2025-04-14 01:48:12 -0300

    new model (User.ts)

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,21 +1,49 @@
-import { Table, Column, Model, DataType, HasMany } from 'sequelize-typescript'
+import { Table, Column, Model, DataType, HasMany, Default, Unique, AllowNull } from 'sequelize-typescript'
 import Budget from './Budget';
 
+/* Los decoradores 'Default', 'Unique' y 'AllowNull' son para definir valores por defecto,  únicos y nulos,
+y deben colocarse antes de la definición de la columna. */ 
+
 @Table({
-    tableName: 'users'
+  tableName: "users",
 })
-
 class User extends Model {
-    @Column({
-        type: DataType.STRING(100),
-    })
-    declare email: string;
+  // Extender Model permite que la clase pueda usar métodos como save(), findAll(), etc.
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING(50),
+  })
+  declare name: string;
 
-    @Column({
-        type: DataType.STRING(60),
-    })
-    declare password: string;
-    
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING(60),
+  })
+  declare password: string;
+
+  @Unique(true)
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING(50),
+  })
+  declare email: string;
+
+  @Column({
+    type: DataType.STRING(6),
+  })
+  declare token: string;
+
+  @Default(false)
+  @Column({
+    type: DataType.BOOLEAN,
+  })
+  declare confirmed: string;
+
+  @HasMany(() => Budget, {
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+  })
+  declare budgets: Budget[];
 }
 
-export default User;    
+export default User;
