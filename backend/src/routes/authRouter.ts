16af61ab16bd3189a08bae5a87b467c7c789527e
Alan S. <lic.sanchezalan@gmail.com>
2025-04-24 10:58:06 -0300

    update route name

--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { body } from 'express-validator';
-import { AuthController } from '../controllers/authController';
+import { AuthController } from '../controllers/AuthController';
 import { handleInputErrors } from '../middleware/validation';
 
 const router = Router();
